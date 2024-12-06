eea5afa19ae3dc1d062d934f0490e0b9f8d7cf15
Infrunami <miguelrojaso2003@gmail.com>
2024-12-06 18:39:49 -0300

    update 1.2
    aAAAAAAAAAAAAAAAAaaaaaaaa

--- appbuses/casther/forms.py
+++ appbuses/casther/forms.py
@@ -1,7 +1,1 @@
 from django import forms # type: ignore
-from .models import Empleado
-
-class EmpleadoForm(forms.ModelForm):
-    class Meta:
-        model = Empleado
-        fields = ['nombre', 'apellido', 'fecha_nacimiento', 'cargo']
